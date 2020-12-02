100a7fc0adef873ffc11dcc8b41b793a59770acb
zahidalidev <m.zahidalidev@gmail.com>
2020-12-03 02:21:03 +0500

    font load

--- components/HomeSlider.js
+++ components/HomeSlider.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { View, ScrollView, Image, StyleSheet, Dimensions, Text } from 'react-native';
 import { Constants } from 'expo';
 import { SliderBox } from "react-native-image-slider-box";
 
+import { useFonts } from 'expo-font'
 const { width } = Dimensions.get('window');
 const height = width * 0.8
 
 import img from "../assets/images/Rectangle6.png"
+import colors from '../config/colors';
 
-class HomeSlider extends Component {
-  state = {
-    images: [
+export default function HomeSlider() {
+    const [images, setImages] = useState([
       img, img, img, img, img         // Local image
-    ]
-  };
-  render() {
-      return (
-        <View
-          style={styles.container}
-        >
-          <SliderBox images={this.state.images} />
-        </View>
-      );
-  }
+    ])
+
+    let [fontsLoaded] = useFonts({
+      'ZermattFirst': require('../assets/fonts/ZermattFirst.otf'),
+    });
+
+    
+    return (
+      <View
+        style={styles.container}
+      >
+        <Text style={styles.sliderHeading} >
+            Recommended for you today
+        </Text>
+        <SliderBox 
+          images={images}
+          dotColor={colors.primary}
+          resizeMode={'cover'}
+        />
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     
   },
+  sliderHeading: {
+    fontFamily: 'ZermattFirst',
+    fontSize: 18,
+    // fontsStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 22,
+    letterSpacing: 0.02,
+    textAlign: 'left',
+
+  }
   
 });
-
-export default HomeSlider;
