30d4c8516d067af7bed6cf98de18bee2621fdfe1
zahidalidev <m.zahidalidev@gmail.com>
2020-12-05 02:45:01 +0500

    profile tutorial responsive

--- components/ListCard.js
+++ components/ListCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Image, Text } from 'react-native';
 import {MaterialCommunityIcons} from "@expo/vector-icons"
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 import colors from '../config/colors';
 
@@ -10,9 +11,9 @@ function ListCard({screenWidth, picture, heading, subHeadding, hashTags, time, s
             <View>
                 <Image source={picture} style={{width: screenWidth/3-15, height: screenWidth/3-15}} />
             </View>
-            <View style={{flexDirection: 'column', left: 15, marginTop: 20}}>
-                <Text style={{lineHeight:25, fontFamily: 'AvianoFlareRegular', fontSize:13, maxWidth: 250}} >{heading}</Text>
-                <Text style={{fontFamily: 'sofiaprolight', fontSize:14, color: colors.primary, marginTop:5}} >{hashTags}</Text>
+            <View style={{flexDirection: 'column', left: 15, marginTop: RFPercentage(1.7)}}>
+                <Text style={{lineHeight:RFPercentage(2.8), fontFamily: 'AvianoFlareRegular', fontSize:RFPercentage(1.6), maxWidth: 250}} >{heading}</Text>
+                <Text style={{fontFamily: 'sofiaprolight', fontSize:RFPercentage(1.6), color: colors.primary, marginTop:5}} >{hashTags}</Text>
                 
                 <View style={{flexDirection: 'row', marginTop: 15, justifyContent: 'flex-end', right: 15}}>
                     <View style={{flexDirection: 'row', right: 15}}>
